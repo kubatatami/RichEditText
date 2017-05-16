222286be7aa3ceefab7d1bd4667b809d8d4730e8
Jakub Bogacki <bogacki.jakub@gmail.com>
2017-05-16 13:27:35 +0200

    bug fix

--- lib/src/main/java/com/github/kubatatami/richedittext/styles/list/TopMarginSpan.java
+++ lib/src/main/java/com/github/kubatatami/richedittext/styles/list/TopMarginSpan.java
@@ -14,8 +14,8 @@ public class TopMarginSpan implements LineHeightSpan.WithDensity, UpdateLayout, 
 
     @Override
     public void chooseHeight(CharSequence text, int start, int end, int spanstartv, int v, Paint.FontMetricsInt fm, TextPaint paint) {
-        if (v == 0) {
-            fm.ascent = (int) (paint.ascent() - ((float)VERTICAL_SPACING * paint.density));
+        if (spanstartv - v == 0) {
+            fm.ascent = (int) (paint.ascent() - ((float) VERTICAL_SPACING * paint.density));
         } else {
             fm.ascent = (int) paint.ascent();
         }

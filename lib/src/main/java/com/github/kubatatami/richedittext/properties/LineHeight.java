0108058526c9d43dc48e4d292c9bd1010ab54e0a
Jakub Bogacki <bogacki.jakub@gmail.com>
2017-06-21 15:43:42 +0200

    support LineHeight static

--- lib/src/main/java/com/github/kubatatami/richedittext/properties/LineHeight.java
+++ lib/src/main/java/com/github/kubatatami/richedittext/properties/LineHeight.java
@@ -1,6 +1,7 @@
 package com.github.kubatatami.richedittext.properties;
 
 import com.github.kubatatami.richedittext.BaseRichEditText;
+import com.github.kubatatami.richedittext.other.DimenUtil;
 import com.github.kubatatami.richedittext.styles.base.StartStyleProperty;
 
 import java.util.Map;
@@ -9,18 +10,25 @@ public class LineHeight implements StartStyleProperty {
 
     @Override
     public String createStyle(BaseRichEditText editText) {
-        if (editText.getLineSpacingMultiplierCompat() == 1f) {
-            return "";
-        } else {
+        if (editText.getLineSpacingMultiplierCompat() != 1f) {
             return "line-height:" + editText.getLineSpacingMultiplierCompat() + ";";
+        } else if (editText.getLineSpacingExtraCompat() > 0) {
+            return "line-height:" + DimenUtil.convertPixelsToDp(editText.getLineSpacingExtraCompat()) + "px;";
+        } else {
+            return "";
         }
     }
 
     @Override
     public boolean setPropertyFromTag(BaseRichEditText editText, Map<String, String> styleMap) {
-        if (styleMap.containsKey("line-height")) {
-            float lineHeight = Float.parseFloat(styleMap.get("line-height"));
-            editText.setLineSpacing(0f, lineHeight);
+        String value = styleMap.get("line-height");
+        if (value != null) {
+            if (value.contains("px")) {
+                float valuePx = Float.parseFloat(value.replace("px", ""));
+                editText.setLineSpacing(DimenUtil.convertDpToPixel(valuePx), 0f);
+            } else {
+                editText.setLineSpacing(0, Float.parseFloat(value));
+            }
             return true;
         }
         return false;
